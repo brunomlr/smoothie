46779551ea5b7c5f3c48ecee5b32a20a431949a0
Bruno Müller <brunohmuller@gmail.com>
2026-01-14 10:08:15 -0800

    change favicon config

--- app/layout.tsx
+++ app/layout.tsx
@@ -87,7 +87,8 @@ export const metadata: Metadata = {
   },
   icons: {
     icon: [
-      { url: "/favicon.ico", sizes: "48x48" },
+      { url: "/favicon.ico" },
+      { url: "/favicon/favicon.svg", type: "image/svg+xml" },
       { url: "/favicon/favicon-96x96.png", sizes: "96x96", type: "image/png" },
       { url: "/favicon/favicon-32x32.png", sizes: "32x32", type: "image/png" },
       { url: "/favicon/favicon-16x16.png", sizes: "16x16", type: "image/png" },
